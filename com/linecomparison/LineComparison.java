808141d11d7e0377eed76717a8833e722a03b1d0
pritamdk <pritamdk1994@gmail.com>
2022-06-27 14:30:20 +0530

    Added: UC3 - Compare two length

--- com/linecomparison/LineComparison.java
+++ com/linecomparison/LineComparison.java
@@ -7,39 +7,41 @@ public class LineComparison {
 	public static void main(String[] args) {
 		
 		
-		double x1, x2, y1, y2, LengthOfLine1;  
-		double x11 = 0, x21 = 0, y11 = 0, y21=0, LengthOfLine2; 
+		double x11, x21, y11, y21, LengthOfLine , LengthOfLine2 ; 
 
 		Scanner scanner = new Scanner(System.in);
-
-
+		
+		
+		System.out.println("Enter the value of x11 :");
+		x11 = scanner.nextDouble();
+		
+		System.out.println("Enter the value of x21 :");
+		x21 = scanner.nextDouble();
+		
+		System.out.println("Enter the value of y11 :");
+		y11 = scanner.nextDouble();
+		
 		System.out.println("Enter the value of y21 :");
 		y21 = scanner.nextDouble();
 
-		LengthOfLine1 = Math.sqrt(  (Math.pow(x21-x11,2))  + (Math.pow(y21-y11,2)) );
-
-		LengthOfLine1 = Math.sqrt(  (Math.pow(x21-x11 , 2))  + (Math.pow(y21-y11 , 2)) );
-		System.out.println(LengthOfLine1);
+		LengthOfLine = Math.sqrt(  (Math.pow(x21-x11 , 2))  + (Math.pow(y21-y11 , 2)) );
+		System.out.println(LengthOfLine);
+		System.out.println("Length of Line 1:" + LengthOfLine);
 
 
 		System.out.println("Enter the value of x11 : ");
-		x11 = scanner.nextDouble(); 
 
-		System.out.println("Enter the value of x21 : ");
-		x21 = scanner.nextDouble(); 
-
-		System.out.println("Enter the value of y11 : ");
-		y11 = scanner.nextDouble(); 
-
-		System.out.println("Enter the value of y21 : ");
 		y21 = scanner.nextDouble(); 
 
 		LengthOfLine2 = Math.sqrt(  (Math.pow(x21-x11, 2))  + (Math.pow(y21-y11, 2)));
 		System.out.println(LengthOfLine2);
+		System.out.println("Length of Line 1:" + LengthOfLine2);
 
 		String s1,s2;
-		s1 = Double.toString(LengthOfLine1);
+		s1 = Double.toString(LengthOfLine);
 		s2 = Double.toString(LengthOfLine2);
 		System.out.println("Equality of two length is " +s1.equals(s2));
+		System.out.println("Equality of two lines using Equal Method : " +s1.equals(s2));
+		System.out.println("Compare of two length is :" + s1.compareTo(s2));
 	}
 }
